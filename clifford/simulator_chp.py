111f372555a0861a4d43e98b7f2dae0f8565f12d
yuyohi <41873114+yuyohi@users.noreply.github.com>
2021-10-31 01:01:13 +0900

    make gate

--- clifford/simulator_chp.py
+++ clifford/simulator_chp.py
@@ -5,20 +5,65 @@ class Simulator:
     def __init__(self, qubit_num: int):
         self.qubit_num = qubit_num
         size = qubit_num * 2
-        self.__stabilizer_tableau = np.concatenate((np.eye(size, dtype=int), np.zeros((size, 1), dtype=int)), axis=1)
+        self._stabilizer_tableau = np.concatenate((np.eye(size, dtype=int), np.zeros((size, 1), dtype=int)), axis=1)
 
     def cx(self, a: int, b: int):
+        """
+        CNOT gate
+        Args:
+            a: control bit
+            b: target bit
+
+        """
         # rを計算
-        self.__stabilizer_tableau[:, self.qubit_num] ^= self.__stabilizer_tableau[:, a] \
-                                                        & self.__stabilizer_tableau[:, self.qubit_num + b - 1] \
-                                                        & (self.__stabilizer_tableau[:, b]
-                                                           ^ self.__stabilizer_tableau[:, self.qubit_num + a - 1] ^ 1)
+        self._stabilizer_tableau[:, self.qubit_num] ^= self._stabilizer_tableau[:, a] \
+                                                       & self._stabilizer_tableau[:, self.qubit_num + b] \
+                                                       & (self._stabilizer_tableau[:, b]
+                                                          ^ self._stabilizer_tableau[:, self.qubit_num + a] ^ 1)
 
         # xを計算
-        self.__stabilizer_tableau[:, b] ^= self.__stabilizer_tableau[:, a]
+        self._stabilizer_tableau[:, b] ^= self._stabilizer_tableau[:, a]
         # zを計算
-        self.__stabilizer_tableau[:, a] ^= self.__stabilizer_tableau[:, b]
+        self._stabilizer_tableau[:, self.qubit_num + a] ^= self._stabilizer_tableau[:, self.qubit_num + b]
+
+    def h(self, a: int):
+        """
+        Hadamard gate
+        Args:
+            a: target bit
+
+        """
+        self.stabilizer_tableau[:, self.qubit_num] ^= self._stabilizer_tableau[:, a] \
+                                                      & self._stabilizer_tableau[:, self.qubit_num + a]
+
+        self._stabilizer_tableau[:, a], self._stabilizer_tableau[:, self.qubit_num + a] \
+            = self._stabilizer_tableau[:, self.qubit_num + a], self._stabilizer_tableau[:, a].copy()
+
+    def s(self, a: int):
+        """
+        S gate (Phase gate)
+        Args:
+            a: target bit
+
+        Returns:
+
+        """
+        self.stabilizer_tableau[:, self.qubit_num] ^= self._stabilizer_tableau[:, a] \
+                                                      & self._stabilizer_tableau[:, self.qubit_num + a]
+
+        self._stabilizer_tableau[:, self.qubit_num + a] ^= self.stabilizer_tableau[:, a]
+
+    def reset_tableau(self):
+        """
+        Reset stabilizer tableau
+        """
+        size = self.qubit_num * 2
+        self._stabilizer_tableau = np.concatenate((np.eye(size, dtype=int), np.zeros((size, 1), dtype=int)), axis=1)
 
     @property
     def stabilizer_tableau(self):
-        return self.__stabilizer_tableau
+        return self._stabilizer_tableau
+
+    @stabilizer_tableau.setter
+    def stabilizer_tableau(self, value: np.ndarray):
+        self._stabilizer_tableau = value
--- tests/test_simulator_chp.py
+++ tests/test_simulator_chp.py
@@ -1,6 +1,7 @@
 from clifford import simulator_chp
 import numpy as np
 
+
 def test_simulator():
     test_sim = simulator_chp.Simulator(10)
     print(test_sim.__stabilizer_tableau)
@@ -15,9 +16,17 @@ def test_cx():
     qubit_num = 2
     sim = simulator_chp.Simulator(qubit_num)
     sim.cx(0, 1)
+    sim.cx(0, 1)
 
     size = qubit_num * 2
     ans = np.concatenate((np.eye(size, dtype=int), np.zeros((size, 1), dtype=int)), axis=1)
-    print(ans)
-    print(sim.stabilizer_tableau)
     assert (sim.stabilizer_tableau == ans).all()
+
+    sim.reset_tableau()
+    sim._stabilizer_tableau[2, 4] = 1
+    sim.cx(0, 1)
+
+    ans_list = [[1, 1, 0, 0, 0], [0, 1, 0, 0, 0], [0, 0, 1, 0, 1], [0, 0, 1, 1, 0]]
+    ans = np.array(ans_list)
+    assert (sim.stabilizer_tableau == ans).all()
+
