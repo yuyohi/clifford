86e382b9109b399464a641dbaf227dc113e6c7a4
yuyohi <41873114+yuyohi@users.noreply.github.com>
2021-11-28 00:08:53 +0900

    sync

--- src/simulator/chp_simulators.rs
+++ src/simulator/chp_simulators.rs
@@ -10,8 +10,21 @@ pub struct Simulator<T: SeedableRng> {
 impl<T: SeedableRng> Simulator<T> {
     pub fn new(qubit_num: usize, rng: T) -> Self {
         let size = qubit_num * 2;
-        let stabilizer_tableau: Array2<u8> = concatenate![Axis(1), Array::eye(size), Array::zeros((size, 1))];
+        let stabilizer_tableau: Array2<u8> =
+            concatenate![Axis(1), Array::eye(size), Array::zeros((size, 1))];
 
-        Simulator::<T> {qubit_num, stabilizer_tableau, rng}
+        Simulator::<T> {
+            qubit_num,
+            stabilizer_tableau,
+            rng,
+        }
+    }
+
+    pub fn cx(&mut self, a: usize, b: usize) {
+        self.stabilizer_tableau.slice_mut(s![.., -1]).assign(
+            (self.stabilizer_tableau.slice(s![.., -1])
+                ^ self.stabilizer_tableau.slice(s![.., a])
+                    * self.stabilizer_tableau.slice(s![.., self.qubit_num + b])).to_owned()
+        )
     }
 }
